b81f2c9e37fd73234dcb462e9ab1286bd612a73c
bbbottle <z@zjh.im>
2020-11-01 12:16:33 +0800

    feat: get access token

--- api/yuque_auth.js
+++ api/yuque_auth.js
@@ -1,17 +1,41 @@
+const axios = require('axios');
+
 const successHtmlWithMsg = (msg) => `
  <html>
     <script>
         if (window.opener) {
            // send them to the opening window
-           window.opener.postMessage(${msg});
+           window.opener.postMessage('${msg}');
            // close the popup
            window.close();
         }
     </script>
  </html>
-`
-module.exports = (req, res) => {
+`;
+
+const YuQueConfig = {
+  tokenURL: 'https://www.yuque.com/oauth2/token',
+  clientID: 'ab7VkEGAfseQU3ecnLNq',
+  clientSecret: 'svLZYV7j0kZ1I9sCEWCXcSBS5Y8TUvHPYo5M2FcR',
+  grantType: 'authorization_code'
+}
+
+const getAccessToken = async (code) => {
+  const tokenInfo = await axios
+    .post(YuQueConfig.tokenURL, {
+      client_id: YuQueConfig.clientID,
+      client_secret: YuQueConfig.clientSecret,
+      code,
+      grant_type: YuQueConfig.grantType
+    })
+
+  return tokenInfo.access_token;
+}
+
+module.exports = async (req, res) => {
+  const code = req.query.code;
+  const accessToken = getAccessToken(code);
   res.setHeader('Content-Type', 'text/html');
-  res.write(successHtmlWithMsg(req.query.code));
+  res.write(successHtmlWithMsg(accessToken));
   res.end();
 }
