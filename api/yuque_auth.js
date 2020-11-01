73cd8e9fb6e85781064eeda3a357b4e56565505d
bbbottle <z@zjh.im>
2020-11-01 11:45:32 +0800

    feat: post msg to parent window

--- api/yuque_auth.js
+++ api/yuque_auth.js
@@ -1,4 +1,17 @@
+const successHtmlWithMsg = (msg) => `
+ <html>
+    <script>
+        if (window.opener) {
+           // send them to the opening window
+           window.opener.postMessage(${msg});
+           // close the popup
+           window.close();
+        }
+    </script>
+ </html>
+`
 module.exports = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html'});
   console.log(req.query);
-  res.send("success");
+  res.send(successHtmlWithMsg(req.query.code));
 }
