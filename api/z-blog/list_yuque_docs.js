b7adae02df4bcf6cd730938d26a49662e5006a76
bbbottle <z@zjh.im>
2020-11-07 11:57:24 +0800

    feat: fetch yuque docs

--- api/z-blog/list_yuque_docs.js
+++ api/z-blog/list_yuque_docs.js
@@ -1,10 +1,33 @@
-module.exports = async (req, res) => {
-  const SDK = require('@yuque/sdk');
-  const client = new SDK({
-    token: req.header.X_Auth_Token
-  });
-  console.log(client.docs.list({
-    namespace: 'ypsyim',
-  }));
-  res.send(200);
+const { cors } = require('./utils');
+const findBlogRepo = (repos) => {
+  return repos.find(({ name }) => name === 'blog');
 };
+
+const fetchYuQueDocs = async (token) => {
+  const SDK = require('@yuque/sdk');
+  const client = new SDK({ token });
+  const { id: user } = await client.users.get();
+  const repos = await client.repos.list({ user });
+  const blogRepo = findBlogRepo(repos);
+  if (!blogRepo) {
+    return [];
+  }
+  const { namespace } = blogRepo;
+  const docs = await client.docs.list({ namespace });
+  return await Promise.all(docs.map(({slug}) => {
+    return client.docs.get({namespace, slug, data: {raw: 1}});
+  }));
+};
+
+const reqHandler = async (req, res) => {
+  const docs = await fetchYuQueDocs(req.headers['x-auth-token']);
+  const posts = docs.map((doc) => {
+    return {
+      title: doc.title,
+      content: doc.body,
+    }
+  })
+  res.json(posts).end();
+}
+
+module.exports = cors(reqHandler);
