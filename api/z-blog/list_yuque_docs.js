8e9ab32e07c4d3464af93dece74f3f0fddeb9708
bbbottle <z@zjh.im>
2020-12-13 17:49:40 +0800

    add: articles api

--- api/z-blog/list_yuque_docs.js
+++ api/z-blog/list_yuque_docs.js
@@ -1,17 +1,7 @@
-const findBlogRepo = (repos) => {
-  return repos.find(({ name }) => name === 'blog');
-};
-
-const fetchYuQueDocs = async (token) => {
+const fetchYuQueDocs = async () => {
   const SDK = require('@yuque/sdk');
-  const client = new SDK({ token });
-  const { id: user } = await client.users.get();
-  const repos = await client.repos.list({ user });
-  const blogRepo = findBlogRepo(repos);
-  if (!blogRepo) {
-    return [];
-  }
-  const { namespace } = blogRepo;
+  const client = new SDK({ token: process.env.YU_QUE_TOKEN });
+  const namespace = 'zjhou/blog';
   const docs = await client.docs.list({ namespace });
   const docsInDetail = await Promise.all(docs.map(({slug}) => {
     return client.docs.get({namespace, slug, data: {raw: 1}});
@@ -20,6 +10,7 @@ const fetchYuQueDocs = async (token) => {
     return {
       title: doc.title,
       content: doc.body,
+      html: doc.body_html,
     }
   })
 };
