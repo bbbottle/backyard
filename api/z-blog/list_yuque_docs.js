050fe30ddc4fe5326002c9989c1c78f7d60d2eb1
bbbottle <z@zjh.im>
2020-12-13 18:16:56 +0800

    refactor: api

--- api/z-blog/list_yuque_docs.js
+++ api/z-blog/list_yuque_docs.js
@@ -1,18 +1,14 @@
-const fetchYuQueDocs = async () => {
+const { cors } = require('../../utils/');
+
+const fetchYuQueDocs = async (req, res) => {
   const SDK = require('@yuque/sdk');
-  const client = new SDK({ token: process.env.YU_QUE_TOKEN });
-  const namespace = 'zjhou/blog';
-  const docs = await client.docs.list({ namespace });
-  const docsInDetail = await Promise.all(docs.map(({slug}) => {
-    return client.docs.get({namespace, slug, data: {raw: 1}});
-  }));
-  return docsInDetail.map((doc) => {
-    return {
-      title: doc.title,
-      content: doc.body,
-      html: doc.body_html,
-    }
-  })
+  const api = new SDK({ token: process.env.YU_QUE_TOKEN });
+  const articles = await api._client.request('repos/zjhou/blog/docs', {});
+  const articlesInDetail = await Promise.all(articles.map(a => api._client.request(`repos/zjhou/blog/docs/${a.slug}`)))
+  res.json(articlesInDetail.map(a => ({
+    title: a.title,
+    content: a.body_html,
+  })));
 };
 
-module.exports = fetchYuQueDocs;
+module.exports = cors(fetchYuQueDocs);
