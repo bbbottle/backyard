3080315023ca28a44b89e2c278331c3756710911
zjhou <z@zjh.im>
2021-02-09 01:40:04 +0800

    feat: 语雀文档格式化

--- 3_rd_party_utils/qu_que_helper/article_formatter.js
+++ 3_rd_party_utils/qu_que_helper/article_formatter.js
@@ -1,4 +1,17 @@
 module.exports = (a) => {
-  const { body_html: content, ...rest } = a;
-  return { content, ...rest };
+  const {
+    body_html: content,
+    id,
+    slug,
+    title,
+    updated_at,
+  } = a;
+
+  return {
+    id,
+    title,
+    slug,
+    content,
+    updated_at
+  }
 };
