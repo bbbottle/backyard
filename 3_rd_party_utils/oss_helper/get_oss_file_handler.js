d3616d14d3435f1efb8397e8ed04152206dcdf83
zjhou <z@zjh.im>
2021-04-24 15:52:29 +0800

    refactor: use json store photos data

--- 3_rd_party_utils/oss_helper/get_oss_file_handler.js
+++ 3_rd_party_utils/oss_helper/get_oss_file_handler.js
@@ -1,18 +1,21 @@
-const OSS = require('ali-oss');
+const OSS = require("ali-oss");
 const client = new OSS({
-  region: 'oss-cn-shenzhen',
+  region: "oss-cn-shenzhen",
   accessKeyId: process.env.ALI_ACCESS_KEY_ID,
   accessKeySecret: process.env.ALI_ACCESS_KEY_SECRET,
-  bucket: 'zjh-im-res',
-  secure: true
+  bucket: "zjh-im-res",
+  secure: true,
 });
 
-const { cors } = require('../../utils');
+const { cors } = require("../../utils");
 
-module.exports = (dir) => cors(async (req, res) => {
-  client.list({
-    prefix: dir
-  }).then((result) => {
-    res.json(result.objects).end();
-  })
-});
+module.exports = (dir, formatter = (n) => n) =>
+  cors(async (req, res) => {
+    client
+      .list({
+        prefix: dir,
+      })
+      .then((result) => {
+        res.json(formatter(result.objects)).end();
+      });
+  });
--- api/photos.js
+++ api/photos.js
@@ -1,3 +1,6 @@
-const { createOSSFileReqHandler } = require('../3_rd_party_utils');
-const { ALI_OSS } = require('../const');
-module.exports = createOSSFileReqHandler(ALI_OSS.IMG_DIR);
+const photos = require("../json_db/photos.json");
+const { cors } = require("../utils");
+
+module.exports = cors((req, res) => {
+  res.json(photos);
+});
